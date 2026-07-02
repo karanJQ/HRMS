d9bbfff227602cfac70ef2b704b481973f0a7aad
Raj Lathigra <rajlathigra03@gmail.com>
2026-07-02 19:06:35 +0530

    UI changes

--- hrms-backend/test-query.js
+++
@@ -1,2 +0,0 @@
-const { pool } = require('./src/config/database');
-pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'kpi_yearly_cycles'").then(res => { console.log(res.rows); process.exit(0); });
